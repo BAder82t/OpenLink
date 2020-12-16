486538d7fe3d08ed29cc986cf19972e2da96e3e9
Bader <bader@venturous.solutions>
2020-12-16 20:16:19 +0000

    added input and buton for login page

--- OpenLink/OpenLink/ClientApp/src/components/LoginPage.js
+++ OpenLink/OpenLink/ClientApp/src/components/LoginPage.js
@@ -1,22 +1,83 @@
 import React, { Component } from 'react';
 
+
+
+
+
+
+
 export class LoginPage extends Component {
 
     static displayName = LoginPage.name;
 
     constructor(props) {
         super(props);
+        this.state = {
+            username: '',
+            password: '',
+            submitted: false
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    render(){
+    handleChange(e) {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
+    }
 
+    handleSubmit(e) {
+        e.preventDefault();
+
+        this.setState({ submitted: true });
+        const { username, password } = this.state;
+        if (username && password) {
+            this.props.login(username, password);
+        }
+    }
+    
+
+ 
+
+    render() {
+        const { loggingIn } = this.props;
+        const { username, password, submitted } = this.state;
         return (
             <div>
-                <h1 id="tabelLabel" >Login</h1>
-                <p>here we login.</p>
+                <div>
+                <input
+                    type="text"
+                      
+                    autoComplete="new-password"
+                    onChange={(event, newValue) => this.setState({ username: newValue })}
+                />
+                    <br />
+                </div>
+                    
+                <br />
+                <div style={{ marginTop: 10 }}>
+                <input
+                    type="password"
+                    hintText="Enter your Password"
+                    floatingLabelText="Password"
+                    onChange={(event, newValue) => this.setState({ password: newValue })}
+                />
+                    <br />
+                </div>
+                <button >Login</button>
+             
             </div>
         );
     }
 }
+function mapState(state) {
+    const { loggingIn } = state.authentication;
+    return { loggingIn };
+}
+
+
+
+
 
 
