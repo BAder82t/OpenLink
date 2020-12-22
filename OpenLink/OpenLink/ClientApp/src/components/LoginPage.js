ea122bb2485eb163e90134b66aa4764c004f9011
Bader <bader@venturous.solutions>
2020-12-22 21:06:17 +0000

    new login design

--- OpenLink/OpenLink/ClientApp/src/components/LoginPage.js
+++ OpenLink/OpenLink/ClientApp/src/components/LoginPage.js
@@ -2,95 +2,109 @@ import React, { Component } from 'react';
 import './Login.scss';
 
 
-
-
-
-
 export class LoginPage extends Component {
 
    
 
     static displayName = LoginPage.name;
 
-   
-
     constructor(props) {
         super(props);
         this.state = {
             username: '',
             password: '',
+            usernameActive:false,
+            passwordActive:false,
             submitted: false
         };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        
         this.updateUsername = this.updateUsername.bind(this);
         this.login = this.login.bind(this);
         this.updatePassword = this.updatePassword.bind(this);
     }
 
-    handleChange(e) {
-        const { name, value } = e.target;
-        this.setState({ [name]: value });
-    }
-
-    handleSubmit(e) {
-        e.preventDefault();
-
-        this.setState({ submitted: true });
-        const { username, password } = this.state;
-        if (username && password) {
-            this.props.login(username, password);
-        }
-    }
+    
     
     
     updateUsername(event) {
+       
 
-        this.setState({ username: event.target.value });
-        console.log("new username = " + this.state.username); 
+        this.setState({ username: event.target.value }, function () { this.checkUsername(); });
+        
     }
+
+    checkUsername()
+    {
+        console.log("new username = " + this.state.username);
+        if (this.state.username == "") {
+            this.setState({ usernameActive: false })
+        } else {
+            this.setState({ usernameActive: true })
+
+        }
+    }
+
+
     updatePassword(event) {
 
-        this.setState({ password: event.target.value });
+        this.setState({ password: event.target.value }, function () { this.checkPassword(); });
+        
+    }
+    checkPassword() {
         console.log("new password = " + this.state.password);
+        if (this.state.password == "") {
+            this.setState({ passwordActive: false })
+        } else {
+            this.setState({ passwordActive: true })
+
+        }
     }
 
+
     login() {
-        console.log("Button clicked password = " + this.state.password + " username =" + this.state.username);
+        
         
     }
  
 
     render() {
-        
-
-        
         return (
-            <div className="login_div">
+            <div className="form">
                 <form>
                     <div>
-                        <h2>Username</h2>
-                    <input
+                        <h2>Login</h2>
+                        <div className="field-wrap">
+                            {
+                                this.state.usernameActive ?
+                                    null : <label> UserName<span className="req">*</span> </label>
+                            }
+                        
+                        <input
                             type="text"
 
                             onChange={this.updateUsername}
-                    />
-                        <br />
+                            />
+                        </div>
+                     
                     </div>
                     
                     <br />
                     <div style={{ marginTop: 10 }}>
-                        <h2>Password</h2>
-                    <input
-                            type="password"
-                            onChange={this.updatePassword}
-                    />
-                        <br />
+                        <div className="field-wrap">
+                            {
+                                this.state.passwordActive ?
+                                    null :<label> Password<span className="req">*</span> </label>
+                            }
+                            <input
+                                type="password"
+                                onChange={this.updatePassword}
+                            />
+                               
+                        </div>
                     </div>
                     <br/>
-                    <button className="button button-block"
-                        onClick={this.login}>Login
+                    <button className="button button-block">Login
                     </button>
                 </form>
             </div>
@@ -98,12 +112,4 @@ export class LoginPage extends Component {
         
     }
 }
-
-
-
-
-
-
-
-
 
